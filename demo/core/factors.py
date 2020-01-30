d12f58baeeb0365227a9b2d499bf3f5367450594
srikanthpragada <srikanthpragada@gmail.com>
2020-01-30 20:55:21 +0530

    30-JAN

---
+++ demo/core/factors.py
@@ -0,0 +1,7 @@
+# Display factors for the given number
+
+num = int(input("Enter a number :"))
+
+for i in range(2, num // 2 + 1):
+    if num % i == 0:
+        print(i)
---
+++ demo/core/for_demo.py
@@ -0,0 +1,7 @@
+for n in range(1, 11, 2):
+    print(n, end=' ')
+
+print("\nNumbers In Reverse\n")
+
+for n in range(10, 0, -1):
+    print(n, end=' ')
