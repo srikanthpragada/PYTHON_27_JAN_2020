91d6c1a7813a9dc3d80fe6c570b2145e27ca1b2b
srikanthpragada <srikanthpragada@gmail.com>
2020-03-05 20:59:25 +0530

    05-MAR

--- webdemo/hr/urls.py
+++ webdemo/hr/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('welcome/', views.welcome),
     path('jobs/', views.list_jobs),
-
+    path('countries/', views.list_countries),
+    path('countryinfo/', views.country_info),
+    path('add_job/', views.add_job),
 ]
+
--- webdemo/hr/views.py
+++ webdemo/hr/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import sqlite3
+import requests
 
 
 def welcome(request):
@@ -15,3 +16,36 @@ def list_jobs(request):
     cur.close()
     con.close()
     return render(request, 'list_jobs.html', {"jobs": jobs})
+
+
+def list_countries(request):
+    resp = requests.get("https://restcountries.eu/rest/v2/all")
+    countries = resp.json()
+    return render(request,'list_countries.html',{'countries' : countries})
+
+
+def country_info(request):
+    code = request.GET["code"]
+    # get info about country with code
+    return render(request,'country_info.html', {})
+
+
+def add_job(request):
+    if 'jobid' in request.GET:
+        # process data as request contains data
+        jobid = request.GET['jobid']
+        jobtitle =request.GET['jobtitle']
+        minsalary = request.GET['minsalary']
+        con = sqlite3.connect(r"e:\classroom\python\jan27\hr.db")
+        cur = con.cursor()
+        cur.execute("insert into jobs values(?,?,?)",
+                    (jobid,jobtitle,minsalary))
+        con.commit()
+        cur.close()
+        con.close()
+        return render(request, "add_job.html",
+                      {'message': "Job has been added succesfully!"})
+        # return redirect("/hr/jobs")
+    else:
+        # Send empty form becuase it is first request
+        return render(request,"add_job.html")
