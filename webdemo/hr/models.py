1f87927891a28f914ecce04159abe82d221de9c0
srikanthpragada <srikanthpragada@gmail.com>
2020-03-09 20:57:49 +0530

    09-MAR

--- webdemo/hr/models.py
+++ webdemo/hr/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    author = models.CharField(max_length=100, null=False)
+    price = models.IntegerField()
+
+    def  __str__(self):
+        return self.title + "," + str(self.price)
